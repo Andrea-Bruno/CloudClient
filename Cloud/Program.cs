9bfc710ac1de7aaab667df571bea396e0377a56a
Andrea Bruno <andrea_bruno@hotmail.com>
2023-02-05 05:40:58 +0200

    update

--- Cloud/Program.cs
+++ Cloud/Program.cs
@@ -1,7 +1,11 @@
 using Cloud;
 using Cloud.Data;
+using EncryptedMessaging;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,14 +18,37 @@ var app = builder.Build();
 
 var configuration = app.Configuration;
 
-string cloudPath = (string)configuration.GetValue(typeof(string), "CloudPath", null);
-string entryPoint = (string)configuration.GetValue(typeof(string), "EntryPoint", null); ; // Used for release
+Static.CloudPath = (string)configuration.GetValue(typeof(string), "CloudPath", null);
+Static.EntryPoint = (string)configuration.GetValue(typeof(string), "EntryPoint", null); ; // Used for release
 #if RELEASE
 if (entryPoint != null && entryPoint.Contains("test")) { Console.WriteLine("WARNING: Test entry point in use: Change entry point in application settings before deployment!"); };
 #endif
 
-var CloudClient = new CloudBox.CloudBox(entryPoint, cloudPath);
-BackupManager.Initialize(CloudBox.CloudBox.LastInstance.CloudPath);
+//var h1 = CloudSync.Util.HashFileName("Download/Sorgenti/Cloud/ClientMobile/cloud-storage-main/node_modules/unimodules-app-loader/android/build/intermediates/aapt_friendly_merged_manifests/debug/aapt/AndroidManifest.xml", false);
+//var h2 = CloudSync.Util.HashFileName("Download/Sorgenti/Cloud/ClientMobile/cloud-storage-main/node_modules/unimodules-app-loader/android/build/intermediates/compiled_local_resources/debug/out", true);
+
+
+var fileLastEntryPoint = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LastEntryPoint");
+if (File.Exists(fileLastEntryPoint))
+{
+    var lastEntryPoint = File.ReadAllText(fileLastEntryPoint);
+    Static.CreateClient(lastEntryPoint);
+}
+
+
+BackupManager.Initialize(CloudBox.CloudBox.GetCloudPath(Static.CloudPath, false));
+
+
+// Functions.ExecuteCommand("cmd.exe", "/C time " + "6:10", false);
+
+#if RELEASE
+// Open the browser
+var url = Environment.GetEnvironmentVariable("ASPNETCORE_URLS")?.Split(";").First();
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+{
+   Functions.ExecuteCommand("cmd.exe", "/C " + "start /max " + url, true);
+}
+#endif
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
---
+++ Cloud/Static.cs
@@ -0,0 +1,29 @@
+﻿using CloudSync;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace Cloud
+{
+    static public class Static
+    {
+        public static string? EntryPoint;
+        public static string? CloudPath;
+        public static readonly bool IsAdmin = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator) : Environment.UserName == "root";
+        public static CloudBox.CloudBox? Client { get; private set; }
+        public static void CreateClient(string? connectToEntryPoint = null)
+        {
+            Client = new CloudBox.CloudBox(CloudPath);
+            if (connectToEntryPoint != null)
+            {
+                Client.CreateContext(connectToEntryPoint);
+            }
+        }
+        public static void Login(string qr, string pin)
+        {
+            if (Client == null)
+                CreateClient();
+            Client?.Login(qr, pin, EntryPoint);
+        }
+    }
+}
